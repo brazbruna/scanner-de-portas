1bcf8ab42263a2d7421ba5fbe2b4e27d6131b21e
Bruna Braz <ssilvabbruna1@gmail.com>
2025-03-24 05:44:46 -0300

    Removendo main.py do rastreamento do Git

--- main.py
+++
@@ -1,8 +0,0 @@
-def minha_funcao(valor1, valor2):
-  return valor1 + valor2
-
-while True:
-  valor1 = int(input("valor1: "))
-  valor2 = int(input("Valor2: "))
-  resposta = minha_funcao(valor1, valor2)
-  print(valor1, "+", valor2, "=", resposta)
